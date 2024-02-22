d38f64237b35b045171d7f7f165f0a41ae84307e
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-02-22 22:26:11 +0700

    first commit

---
+++ src/main/java/iegcode/sequenced/collaction/JavaSequencedCollactionApplication.java
@@ -0,0 +1,13 @@
+package iegcode.sequenced.collaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSequencedCollactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSequencedCollactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iegcode/sequenced/collaction/JavaSequencedCollactionApplicationTests.java
@@ -0,0 +1,13 @@
+package iegcode.sequenced.collaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSequencedCollactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
